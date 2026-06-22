dfaca9e9ba45718a83e15336cd4c0b13117890e0
Danny Sauer <danny.sauer@herodevs.com>
2026-06-22 17:31:21 -0500

    Resolve pipx-installed Vexcalibur command

--- scripts/run-vexcalibur.sh
+++ scripts/run-vexcalibur.sh
@@ -8,27 +8,44 @@ skip_install="${VEXCALIBUR_SKIP_INSTALL:-false}"
 
 if [[ "$skip_install" != "true" ]]; then
   python -m pip install --user pipx
-  pipx_bin="${PIPX_BIN:-$HOME/.local/bin/pipx}"
-  "$pipx_bin" install --force "$package_spec"
+  python -m pipx install --force "$package_spec"
 fi
-
-vexcalibur_bin="${VEXCALIBUR_BIN:-$HOME/.local/bin/vexcalibur}"
 
 case "$command_name" in
   help)
-    "$vexcalibur_bin" --help
     ;;
   query-osv)
     if [[ -z "$purls" ]]; then
       echo "purls input is required when command is query-osv" >&2
       exit 2
     fi
-
-    mapfile -t purl_args <<<"$purls"
-    "$vexcalibur_bin" query-osv "${purl_args[@]}"
     ;;
   *)
     echo "unsupported command: $command_name" >&2
     exit 2
+    ;;
+esac
+
+vexcalibur_bin="${VEXCALIBUR_BIN:-}"
+if [[ -z "$vexcalibur_bin" ]]; then
+  if command -v vexcalibur >/dev/null 2>&1; then
+    vexcalibur_bin="vexcalibur"
+  elif [[ -x "$HOME/.local/bin/vexcalibur" ]]; then
+    vexcalibur_bin="$HOME/.local/bin/vexcalibur"
+  elif [[ -x "/opt/pipx_bin/vexcalibur" ]]; then
+    vexcalibur_bin="/opt/pipx_bin/vexcalibur"
+  else
+    echo "vexcalibur executable was not found after installation" >&2
+    exit 127
+  fi
+fi
+
+case "$command_name" in
+  help)
+    "$vexcalibur_bin" --help
+    ;;
+  query-osv)
+    mapfile -t purl_args <<<"$purls"
+    "$vexcalibur_bin" query-osv "${purl_args[@]}"
     ;;
 esac
